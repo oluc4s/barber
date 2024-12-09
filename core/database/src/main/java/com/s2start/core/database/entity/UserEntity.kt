d02dc25a9a20011c8d886562a8bdf746a6f3f865
oluc4s <mateuslucas.ti@gmail.com>
2024-12-09 19:08:50 -0300

    feat: ajuste user entity

--- core/database/src/main/java/com/s2start/core/database/entity/UserEntity.kt
+++ core/database/src/main/java/com/s2start/core/database/entity/UserEntity.kt
@@ -1,11 +1,13 @@
 package com.s2start.core.database.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class UserEntity (
+    @PrimaryKey
     val uid: String,
     val email: String,
     val displayName: String,
-    val phoneNumber: String
+    val phoneNumber: String,
 )
